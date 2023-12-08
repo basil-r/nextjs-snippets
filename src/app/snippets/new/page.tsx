8e160bb6d854d64c2a5ffe966ee8e41151c9344b
basil-r <potamibasil@gmail.com>
2023-12-08 21:11:54 +0200

    first commit

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,33 @@
+export default function SnippetsCreatePage() {
+  return (
+    <form action="">
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            id="title"
+            className="border rounded p-2 w-full"
+            name="title"
+            type="text"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            id="code"
+            className="border rounded p-2 w-full"
+            name="code"
+          />
+        </div>
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
